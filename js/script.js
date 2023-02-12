6067026c9d0f9d85bb401f01ddad58aeb7955918
vansh <yadavsatpal544@gmail.com>
2023-02-13 00:11:41 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,141 @@
+// taking all button from indexedDB.html
+let A = document.getElementById('A')
+let B = document.getElementById('B')
+let C = document.getElementById('C')
+let D = document.getElementById('D')
+let E = document.getElementById('E')
+let F = document.getElementById('F')
+let G = document.getElementById('G')
+let H = document.getElementById('H')
+let I = document.getElementById('I')
+let J = document.getElementById('J')
+let K = document.getElementById('K')
+let L = document.getElementById('L')
+let M = document.getElementById('M')
+let N = document.getElementById('N')
+let O = document.getElementById('O')
+let P = document.getElementById('P')
+let Q = document.getElementById('Q')
+let R = document.getElementById('R')
+let S = document.getElementById('S')
+let T = document.getElementById('T')
+let U = document.getElementById('U')
+let V = document.getElementById('V')
+let W = document.getElementById('W')
+let X = document.getElementById('X')
+let Y = document.getElementById('Y')
+let Z = document.getElementById('Z')
+let Pause = document.getElementById('Pause')
+// taking audio
+let As = new Audio('../sounds/a.mp3')
+let Bs = new Audio('../sounds/b.mp3')
+let Cs = new Audio('../sounds/c.mp3')
+let Ds = new Audio('../sounds/d.mp3')
+let Es = new Audio('../sounds/e.mp3')
+let Fs = new Audio('../sounds/f.mp3')
+let Gs = new Audio('../sounds/g.mp3')
+let Hs = new Audio('../sounds/h.mp3')
+let Is = new Audio('../sounds/i.mp3')
+let Js = new Audio('../sounds/j.mp3')
+let Ks = new Audio('../sounds/k.mp3')
+let Ls = new Audio('../sounds/l.mp3')
+let Ms = new Audio('../sounds/m.mp3')
+let Ns = new Audio('../sounds/n.mp3')
+let Os = new Audio('../sounds/o.mp3')
+let Ps = new Audio('../sounds/p.mp3')
+let Qs = new Audio('../sounds/q.mp3')
+let Rs = new Audio('../sounds/r.mp3')
+let Ss = new Audio('../sounds/s.mp3')
+let Ts = new Audio('../sounds/t.mp3')
+let Us = new Audio('../sounds/u.mp3')
+let Vs = new Audio('../sounds/v.mp3')
+let Ws = new Audio('../sounds/w.mp3')
+let Xs = new Audio('../sounds/x.mp3')
+let Ys = new Audio('../sounds/y.mp3')
+let Zs = new Audio('../sounds/z.mp3')
+let Pauses = new Audio('../sounds/pause')
+
+
+
+// adding a function to run music
+A.addEventListener('click', ()=>{
+    As.play();
+})
+B.addEventListener('click', ()=>{
+    Bs.play();
+})
+C.addEventListener('click', ()=>{
+    Cs.play();
+})
+D.addEventListener('click', ()=>{
+    Ds.play();
+})
+E.addEventListener('click', ()=>{
+    Es.play();
+})
+F.addEventListener('click', ()=>{
+    Fs.play();
+})
+G.addEventListener('click', ()=>{
+    Gs.play();
+})
+H.addEventListener('click', ()=>{
+    Hs.play();
+})
+I.addEventListener('click', ()=>{
+    Is.play();
+})
+J.addEventListener('click', ()=>{
+    Js.play();
+})
+K.addEventListener('click', ()=>{
+    Ks.play();
+})
+L.addEventListener('click', ()=>{
+    Ls.play();
+})
+M.addEventListener('click', ()=>{
+    Ms.play();
+})
+N.addEventListener('click', ()=>{
+    Ns.play();
+})
+O.addEventListener('click', ()=>{
+    Os.play();
+})
+P.addEventListener('click', ()=>{
+    Ps.play();;
+})
+Q.addEventListener('click', ()=>{
+    Qs.play();
+})
+R.addEventListener('click', ()=>{
+    Rs.play();
+})
+S.addEventListener('click', ()=>{
+    Ss.play();
+})
+T.addEventListener('click', ()=>{
+    Ts.play();
+})
+U.addEventListener('click', ()=>{
+    Us.play();
+})
+V.addEventListener('click', ()=>{
+    Vs.play();
+})
+W.addEventListener('click', ()=>{
+    Ws.play();
+})
+X.addEventListener('click', ()=>{
+    Xs.play();
+})
+Y.addEventListener('click', ()=>{
+    Ys.play();
+})
+Z.addEventListener('click', ()=>{
+    Zs.play();
+})
+Pause.addEventListener('click', ()=>{
+    Pauses.play();
+});
